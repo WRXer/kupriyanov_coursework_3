d8443df5ede69966a73c3adde5626e5899ad25eb
WRXer <king_311@mail.ru>
2023-02-17 12:12:35 +0400

    Вызвали класс и вписали в основную функцию

--- functions.py
+++ functions.py
@@ -1,6 +1,6 @@
 import json
 from _datetime import datetime
-
+from operations import Operations
 
 def load_words():
     """
@@ -9,61 +9,7 @@ def load_words():
     with open('operations.json', 'r', encoding='utf-8') as file:
         raw_json = file.read()
         words = json.loads(raw_json)
-        return words
-
-
-def user_id():
-    """
-    Функция проверки правильного ввода айди клиента
-    """
-    while True:
-        #user_input = input("Введите айди пользователя")   #Открыть при сдаче
-        user_input = '587085106'
-        if user_input.isdigit():
-            return user_input
-            break
-        else:
-            print("Введите айди в цифровом формате!")
-            continue
-
-
-def one_user_story(words,user,count):
-    """
-    Функция загрузки данных одного пользователя
-    """
-    one_user_story = []
-    for w in words:
-        if int(user) == w["id"]:
-            count +=1
-            one_user_story.append(w)
-            return one_user_story,count
-        else:
-            continue
-
-
-def sorted_date(words):
-    """
-    Функция сортировке по дате(сверху самые последние)
-    """
-    my_dict = {}
-    for w in words:
-        if not w:    #Ищем пустые словари
-            continue
-        else:
-            if w['state'].lower() == "executed":    #Условие, что операция выполнена
-                y = []
-                c = 0
-                for i in w['date']:
-                    if i.isdigit() and c < 12:
-                        y.append(i)
-                        c +=1
-                    else:
-                        continue
-                w['data'] = int("".join(y))
-                my_dict[w['id']] = w['data']
-            else:
-                continue
-    return dict(sorted(my_dict.items(), key=lambda item: item[1], reverse=True))
+        return Operations(words=words)
 
 
 def last_operations(sorted_date_d):
@@ -97,6 +43,7 @@ def finish_list(words, l_o):
 
 
 def main():
-    words = load_words()  # Загружаем бд
-    sorted_date_d = sorted_date(words)
-    l_o = last_operations(sorted_date_d)  # Last operations
+    operation = load_words()  # Загружаем бд
+    operation.sorted_date()
+    operation.last_operations()
+
