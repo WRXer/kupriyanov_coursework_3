2063ed2917f4dda909724cb5b55abf1cc996373d
WRXer <king_311@mail.ru>
2023-02-17 12:20:13 +0400

    Вписали в основную функцию готовый к выводу список

--- functions.py
+++ functions.py
@@ -12,38 +12,10 @@ def load_words():
         return Operations(words=words)
 
 
-def last_operations(sorted_date_d):
-    """
-    Функция определения последних 5 операций
-    """
-    count = 0
-    l_o = {}
-    for k,v in sorted_date_d.items():
-        if count < 5:
-            count += 1
-            l_o[k] = v
-    return l_o
-
-
-def finish_list(words, l_o):
-    """
-    Окончательный список
-    """
-    finish_list = []
-    for l in l_o.keys():
-        for w in words:
-            if not w:  # Ищем пустые словари
-                continue
-            else:
-                if l == w['id']:
-                    finish_list.append(w)
-                else:
-                    continue
-    return finish_list
-
-
 def main():
-    operation = load_words()  # Загружаем бд
-    operation.sorted_date()
-    operation.last_operations()
+    operation = load_words()    #Загружаем бд
+    operation.sorted_date()    #Сортируем по дате
+    operation.last_operations()    #Находим последние 5 операций
+    operation.finish_list()    #Готовим к выводу
+
 
